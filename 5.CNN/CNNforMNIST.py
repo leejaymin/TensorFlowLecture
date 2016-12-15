5e434ad4f70540ca8def372a19a4862f7b5b34d7
Jemin Lee <leejaymin@cnu.ac.kr>
2016-12-16 00:27:31 +0900

    update comments

--- 5.CNN/CNNforMNIST.py
+++ 5.CNN/CNNforMNIST.py
@@ -18,11 +18,13 @@ def model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden):
                         strides=[1, 2, 2, 1], padding='SAME')
     l1 = tf.nn.dropout(l1, p_keep_conv)
 
+
     l2a = tf.nn.relu(tf.nn.conv2d(l1, w2,                     # l2a shape=(?, 14, 14, 64)
                         strides=[1, 1, 1, 1], padding='SAME'))
     l2 = tf.nn.max_pool(l2a, ksize=[1, 2, 2, 1],              # l2 shape=(?, 7, 7, 64)
                         strides=[1, 2, 2, 1], padding='SAME')
     l2 = tf.nn.dropout(l2, p_keep_conv)
+
 
     l3a = tf.nn.relu(tf.nn.conv2d(l2, w3,                     # l3a shape=(?, 7, 7, 128)
                         strides=[1, 1, 1, 1], padding='SAME'))
@@ -30,6 +32,7 @@ def model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden):
                         strides=[1, 2, 2, 1], padding='SAME')
     l3 = tf.reshape(l3, [-1, w4.get_shape().as_list()[0]])    # reshape to (?, 2048)
     l3 = tf.nn.dropout(l3, p_keep_conv)
+
 
     l4 = tf.nn.relu(tf.matmul(l3, w4))
     l4 = tf.nn.dropout(l4, p_keep_hidden)
